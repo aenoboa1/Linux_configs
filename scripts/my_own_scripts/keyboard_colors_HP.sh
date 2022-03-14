9fe28a53420c8279304e115430aa071dd5f0646d
aenoboa1 <aenoboa1@espe.edu.ec>
2022-03-14 11:18:11 -0500

    configs

--- scripts/my_own_scripts/keyboard_colors_HP.sh
+++ scripts/my_own_scripts/keyboard_colors_HP.sh
@@ -5,32 +5,9 @@
 function read_image()
 {
     echo "HP OMEN 15-DC keyboard colors script " | lolcat -as 100
-    read -p 'Please , input the image path:' image_path
+    image_path=$(zenity --entry --text="Please , input the image path:")
 
-
-    #prompt="Please select a image file:"
-    #options=( $(find -maxdepth 1 -print0 | xargs -0) )
-
-    #PS3="$prompt "
-    #select opt in "${options[@]}" "Quit" ; do 
-    #    if (( REPLY == 1 + ${#options[@]} )) ; then
-    #        exit
-
-    #    elif (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
-    #        echo  "You picked $opt which is file $REPLY"
-    #        break
-
-    #    else
-    #        echo "Invalid option. Try another one."
-    #    fi
-    #done    
-
-    #ls -ld $opt
-
-    #image_path=$(readlink -f "$opt")
-
-
-    pattern=".*\.(gif|jpe?g|bmp|png)$"
+    pattern=".*\.(gif|jpe?g|bmp|png|webp)$"
     
     if [[ $image_path =~ $pattern ]]; then
         
@@ -38,12 +15,12 @@ function read_image()
        
         # create wal cache colors 
         sudo -u $SUDO_USER bash -c "wal -q -n -i '$image_path'"
-        #sudo -u $SUDO_USER bash "wal -n -i '$image_path'"
         
         if [ $? -eq 0 ]; then
             echo "******* Created cache colors path ******* "
         else
            echo FAIL
+           exit 0
         fi
 
         image=${image_path##*/}  
@@ -67,9 +44,5 @@ function read_image()
         echo "Not an image"
     fi
 }
+
 read_image
-
-
-
-
-
